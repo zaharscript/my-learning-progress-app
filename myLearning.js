67735e4758ea5298f883a8cd6e15290b54b3d515
Zahar <zahratulnazirah@gmail.com>
2023-10-21 00:57:26 +0800

    change id and class
    rewrite all id and class to  proper name resemble the project purpose

--- myLearning.js
+++ myLearning.js
@@ -1,60 +1,62 @@
 // Create a data structure to store the tasks and their hierarchy
-const tasks = [];
+const myCourse = [];
 
-function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const taskText = taskInput.value.trim();
+function addCourse() {
+    const courseName = document.getElementById("courseName");
+    const course = courseName.value.trim();
 
-    if (taskText === "") {
+    if (course === "") {
         return;
     }
 
-    const taskList = document.getElementById("taskList");
+    const courseList = document.getElementById("courseList");
     const li = document.createElement("li");
     li.innerHTML = `
+    <div class="topic">
+        <span>${course}</span>
         <input type="checkbox">
-        <span>${taskText}</span>
-        <button class="subtask" onclick="addSubtask(this)">Add Sub Topic</button>
+        <button class="subtask" onclick="addSubTopic(this)">Add Topic</button>
+    </div>
     `;
 
-    taskList.appendChild(li);
+    courseList.appendChild(li);
 
     // Create a task object and add it to the tasks array
-    const taskObject = {
-        text: taskText,
-        subtasks: [],
+    const courseObject = {
+        text: course,
+        subTopic: [],
     };
-    tasks.push(taskObject);
+    myCourse.push(courseObject);
 
-    taskInput.value = "";
+    courseName.value = "";
 }
 
 function toggleCompleted(button) {
     button.classList.toggle("completed");
 }
 
-function addSubtask(button) {
-    const parentTask = button.parentElement;
-    const parentTaskText = parentTask.querySelector("span").textContent;
+function addSubTopic(button) {
+    const parentCourse = button.parentElement;
+    const parentTaskText = parentCourse.querySelector("span").textContent;
 
-    const subtaskText = prompt("Enter subtask:");
+    const subTopicText = prompt("Enter Topic:");
 
-    if (subtaskText === null || subtaskText.trim() === "") {
+    if (subTopicText === null || subTopicText.trim() === "") {
         return;
     }
 
     // Find the corresponding task in the tasks array and add the subtask
-    const taskObject = tasks.find(task => task.text === parentTaskText);
-    taskObject.subtasks.push(subtaskText);
+    const taskObject = myCourse.find(task => task.text === parentTaskText);
+    taskObject.subTopic.push(subTopicText);
 
     const ul = document.createElement("ul");
-    ul.innerHTML = `    <li><input type="checkbox" class="done"><span>${subtaskText}</span></li>`;
+    ul.innerHTML = `    <li><span>${subTopicText}</span><input type="checkbox" class="done"></li>`;
 
-    parentTask.appendChild(ul);
+    parentCourse.appendChild(ul);
 }
 
-document.getElementById("taskInput").addEventListener("keypress", function(event) {
+document.getElementById("courseName").addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        addTask();
+        addCourse();
     }
 });
