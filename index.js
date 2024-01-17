eeb336ab2254764b121da23c48f43f7f7773f049
Zahar <zahratulnazirah@gmail.com>
2024-01-17 13:31:11 +0800

    deleting  file
    core package module and library.png
    legasiCode.html
    myLearning.css

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 //database connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 mongoose.connect(process.env.DB_URI, { 
     // useNewUrlParser: true, 
     // useUnifiedTopology:true
@@ -53,9 +80,6 @@ app.set("views", "./views");
 //routes prefix
 app.use("", require("./routes/routes"));
 
-// app.get("/",(req,res)=>{
-//     res.send("Hellow World");
-// })
 
 app.listen(PORT, ()=>{
     console.log(`Connection successful establish at http://localhost:${PORT}`);
