c97e3eb8eb2c46395862630cf06cf8c6313c1264
Zahar <zahratulnazirah@gmail.com>
2024-08-01 00:54:15 +0800

    adjusting footer and header
    adjusting header and footer to retrieve icon and image

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //imports
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
 // const bootstrap =require('bootstrap');
 
 const app = express();
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 //database connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.DB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,7 +31,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Deployment pinged. Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -38,49 +39,45 @@ async function run() {
 }
 run().catch(console.dir);
 
-mongoose.connect(process.env.DB_URI, { 
-    // useNewUrlParser: true, 
-    // useUnifiedTopology:true
+mongoose.connect(process.env.DB_URI, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology:true
 });
 const db = mongoose.connection;
-db.on('error',(error)=>console.log(error));
-db.once('open',()=>{
-    console.log("Connected to MongoDB");
+db.on("error", (error) => console.log(error));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'))
-app.use(express.static('javascript'));
-
-
+app.use(methodOverride("_method"));
+app.use(express.static("javascript"));
 
 app.use(
-    session({
-        secret: 'secret',
-        resave: false,
-        saveUninitialized: true,
-    })
-    
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
-app.use((req,res,next)=>{
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next();
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
 });
 
 //save templete engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 app.set("views", "./views");
 
 //routes prefix
 app.use("", require("./routes/routes"));
 
-
-app.listen(PORT, ()=>{
-    console.log(`Connection successful establish at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Connection successful establish at http://localhost:${PORT}`);
+});
